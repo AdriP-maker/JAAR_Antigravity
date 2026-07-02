4bde4e0d30c92e832b2e514e219a89e05328b263
Mipro <Mipro@DESKTOP-QN52C2T>
2026-07-01 20:25:12 -0500

    fix: set vite base to root for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/JAAR_Antigravity/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
